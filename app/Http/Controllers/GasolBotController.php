e9becb192af474de603121ccb75973314ae6c527
Ashley7 <ashley7520charles@gmail.com>
2026-02-25 14:27:31 +0300

    Improves the state Machine

--- app/Http/Controllers/GasolBotController.php
+++ app/Http/Controllers/GasolBotController.php
@@ -24,23 +24,39 @@ class GasolBotController extends Controller
 
          Log::info($phone." -".$text."-".$button);
 
-        // Reset flow
+       // Reset flow
         if ($text === 'menu') {
-            GasolBot::updateOrCreate(
+            $session = GasolBot::updateOrCreate(
                 ['phone' => $phone],
-                ['state' => 'START', 'meter_number' => null, 'amount' => null]
+                [
+                    'state' => 'MENU',
+                    'meter_number' => null,
+                    'amount' => null,
+                ],
             );
+
+            $this->sendMenuButtons($phone);
+            return response()->json(['status' => 'received'], 200);
         }
 
         $session = GasolBot::firstOrCreate(
             ['phone' => $phone],
-            ['state' => 'START']
+            ['state' => 'MENU']
         );
 
-        /* ================= START ================= */
-        if ($session->state === 'START') {
+        // Define active process states
+        $activeStates = [
+            'ENTER_METER',
+            'ENTER_AMOUNT',
+            'ENTER_PHONE_NUMBER',
+            'HELP'
+        ];
+
+        // If user is NOT in a process → show menu
+        if (!in_array($session->state, $activeStates)) {
             $this->sendMenuButtons($phone);
             $session->update(['state' => 'MENU']);
+
             return response()->json(['status' => 'received'], 200);
         }
 
