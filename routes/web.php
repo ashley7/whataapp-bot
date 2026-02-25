8bcbd0fa3aeead56c199e76bd9041c3b71d13562
Ashley7 <ashley7520charles@gmail.com>
2026-02-25 10:21:34 +0300

    Adds URLs

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
+use App\Http\Controllers\GasolBotController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+ 
+//Whatsapp endpoints
+Route::post('whatsapp_webhook', [GasolBotController::class, 'handle']);
+Route::get('whatsapp_webhook', [GasolBotController::class, 'webhook']);
 
 Route::get('/', function () {
     return view('welcome');
