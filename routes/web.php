91d8bdb5cf1699b1ab4aaef918afc0e9f05cfa15
Ashley7 <ashley7520charles@gmail.com>
2026-02-25 11:38:48 +0300

    Clean ups

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 //Whatsapp endpoints
 Route::post('whatsapp_webhook', [GasolBotController::class, 'handle']);
 Route::get('whatsapp_webhook', [GasolBotController::class, 'webhook']);
+Route::get('url',[GasolBotController::class,'url']);
 
 Route::get('/', function () {
     return "This is a whatsapp bot setup ..";
